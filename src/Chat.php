be05c8968ab5d0dfc9962bd3470242928a099f28
FalloutBaby <ferrashe4ka@mail.ru>
2018-07-14 21:03:46 +0300

    Сообщения появляются у всех, в т.ч. и у нас, отправка происходит в виде: пользователь, сообщение. В данных о пользователе пока null

--- src/Chat.php
+++ src/Chat.php
@@ -19,14 +19,17 @@ class Chat implements MessageComponentInterface {
 
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+		
+        $message = [
+            'text' => $msg,
+            'user' => $conn->resourceId,
+        ];
+		
+		echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
             , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
         foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-            }
+                $client->send(json_encode($message));
         }
     }
 
