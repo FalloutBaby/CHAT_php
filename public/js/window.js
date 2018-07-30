28d2d3d69e886e25429c9ce3184fd2b28f1f28fa
FalloutBaby <ferrashe4ka@mail.ru>
2018-07-30 15:51:22 +0300

    added a 'connections' window

--- public/js/window.js
+++ public/js/window.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 var conn = new WebSocket('ws://localhost:8080');
 conn.onopen = function(e) {
     console.log("Connection established!");
+	$('#connections').append("<div class='connected_user'><h4 class='user_name'>"+this.name+"</h4></div>");
 };
 
 conn.onmessage = function(e) {
 	var messages = JSON.parse(e.data);
+	console.log(messages);
 	$('#chat_output').append("<div><h4 class='user_name'>"+messages['user']+"</h4><p class='message_inc'>"+messages['text']+"</p></div>");
 	var block = $('#chat_output');
   	block.scrollTop(block.prop('scrollHeight'));
-=======
-var conn = new WebSocket('ws://localhost:8080');
-conn.onopen = function(e) {
-    console.log("Connection established!");
-};
-
-conn.onmessage = function(e) {
-	var messages = JSON.parse(e.data);
-	$('#chat_output').append("<div><h4 class='user_name'>"+messages['user']+"</h4><p class='message_inc'>"+messages['text']+"</p></div>");
-	var block = $('#chat_output');
-  	block.scrollTop(block.prop('scrollHeight'));
->>>>>>> origin/master
 };
