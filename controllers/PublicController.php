0c26cb7165bac7849f51bbce6ec7e54eb4c4671c
FalloutBaby <ferrashe4ka@mail.ru>
2018-07-30 15:50:09 +0300

    little modifications

--- controllers/PublicController.php
+++ controllers/PublicController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Controllers;
@@ -56,63 +55,9 @@ class PublicController extends Controller
 		
 		$this->render('public/index', ['account' => $account,] );
 	}
-=======
-<?php
-
-namespace Controllers;
-
-use MyApp\Auth;
-use MyApp\Controller;
-use Models\Account;
-use Models\User;
-
-class PublicController extends Controller
-{
-    public function actionIndex()
-    {
-        if(Auth::check()) {
-            echo " Привет, " . $_SESSION['user'] . "! ";
-        }
-        $accountModel = new Account();
-		
-        $user = new User;
-		
-        $account = $accountModel->getAccount();
-		
-		echo " Привет, " . $_SESSION['user'] . "! ";
-		
-        $this->render('public/index', ['account' => $account,]);
-    }
 	
-	
-    public function actionRegister()
-    {
-        $accountModel = new Account();
-		$accountModel->request = $this->requestArray();
-		
-        $account = $accountModel->registerAccount();
-		
-		echo $this->render('public/index', ['account' => $account,] );
-	}
-
-    public function actionLogin()
-    {
-		$accountModel = new Account();
-		$accountModel->request = $this->requestArray();
-
-        $account = $accountModel->loginAccount();
-		
-		echo $this->render('public/index', ['account' => $account,] );
-    }
-	
-    public function actionLogout()
+    public function actionSession()
 	{
-		$accountModel = new Account();
-		$accountModel->request = $this->requestArray();
-
-        $account = $accountModel->accountLogout();
-		
-		$this->render('public/index', ['account' => $account,] );
+     echo json_encode($_SESSION);
 	}
->>>>>>> origin/master
 }
